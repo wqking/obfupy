3ac0b329c0ce26eb582ea8e7db0d5f2392f02797
wqking <wqking@outlook.com>
2024-05-26 08:37:34 +0800

    Removed transformers ByteEncoder and EncodeBase64, they are replaced by Codec

--- obfupy/transformers/byteencoder.py
+++
@@ -1,5 +0,0 @@
-from .internal.ibyteencoder import _IByteEncoder
-
-class ByteEncoder :
-	def transform(self, documentManager) :
-		_IByteEncoder().transform(documentManager)
--- test/test.py
+++ test/test.py
@@ -6,10 +6,8 @@ import codecs
 from obfupy.documentmanager import DocumentManager
 from obfupy.document import Document
 import obfupy.util as util
-from obfupy.transformers.encodebase64 import EncodeBase64
 from obfupy.transformers.rewriter import Rewriter
 from obfupy.transformers.literal import Literal
-from obfupy.transformers.byteencoder import ByteEncoder
 from obfupy.transformers.replacer import Replacer
 from obfupy.transformers.codec import Codec
 from obfupy.transformers.codec import CodecProvider
@@ -21,7 +19,6 @@ documentManager.addDocument(util.loadDocumentsFromFiles(util.findFiles('input'))
 #Rewriter().transform(documentManager)
 #Replacer(symbols = [ 'n', 'makeMessage' ]).transform(documentManager)
 #Literal(addExtraSpaces = True, expandIndent = True).transform(documentManager)
-#ByteEncoder().transform(documentManager)
 provider = CodecProvider(encoder = lambda x : codecs.encode(x, 'zip'), decoder = "codecs.decode(%s, 'zip')", extraCode = 'import codecs')
 #provider = CodecProvider()
 Codec(codecproviders.zip).transform(documentManager)
