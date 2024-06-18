913c689e0ec1b6ddd673ae1099fb39917baae749
wqking <wqking@outlook.com>
2024-06-18 14:40:52 +0800

    Removed wrongly committed output files

--- tests/main/output/test_main.py
+++
@@ -1,9 +0,0 @@
-import importa
-
-def Il1I1II1IIIl():
-    Ill1I1II11l1_a_50826 = importa.ClassA()
-    Ill1I1II11l1_a_50826.sayHello()
-    lllll1l1llII_arg_90498 = importa.ArgumentParser()
-
-def test_main():
-    return Il1I1II1IIIl()
